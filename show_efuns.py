c92f7c080bea0e9238e575f34f4acaea7f714ec1
kczarn <khczarnowski@gmail.com>
2016-09-04 22:55:26 +0200

    Some cleanup in show_efuns.py. Improved 2-dim embedding presentation.

--- show_efuns.py
+++ show_efuns.py
@@ -4,63 +4,74 @@
 #
 
 import random
+from math import sqrt
 from matplotlib import pyplot as plt
 import matplotlib.colors as colors
 import matplotlib.cm as cm
 
 
-#gray = plt.get_cmap("gray")
-gray_r = plt.get_cmap("gray_r")
-seismic = plt.get_cmap("seismic")  #"rainbow" "jet" "bwr" "coolwarm" "seismic")
+# color coding of edge weights
+weightcolor = cm.ScalarMappable(
+    norm=colors.Normalize(vmin=0.0, vmax=1.0),
+    cmap=plt.get_cmap("gray_r")
+).to_rgba
+
+# color coding of eigenfunctions on vertices -- normalization is dynamic, so we parametrize with norm:
+efcolor = (
+    lambda norm: cm.ScalarMappable(
+        norm=norm,
+        cmap=plt.get_cmap("seismic")
+    ).to_rgba
+)
+
 
 random.seed(12345678)
-
 def rnoise(rn, N):
     return [ 1.0 + 2.0 * rn * (random.random() - 0.5) for i in range(N) ]
 
 
-def show_efuns(ax, ef1, ef2, W, D):
+# helpers for managing the radial scale of distribution
+def rmax(xs, ys):
+    return sqrt((xs**2 + ys**2).max())
+
+
+def show_efuns(ax, f1, f2, efuns, truth, W, D):
     N, M = W.shape
-    assert N == M and D.shape == (N, N)
-    assert len(ef1) == N and len(ef2) == N  
-    D1 = [ D[i,i] for i in range(N) ]
-    ax.scatter(ef1, ef2, c=D1, s=200)
-    Wnorm = colors.Normalize(vmin=0.0, vmax=W.max())
-    Wcolor = cm.ScalarMappable(norm=Wnorm, cmap=jet).to_rgba
+    assert N == M and D.shape == (N, N) and efuns.shape == (N, N)
+    ef1 = efuns[:,f1] ; ef2 = efuns[:,f2]
+    maxD = D.max()
+    Dn = [ D[i,i] / maxD for i in range(N) ]  # normalized vertex degrees as a vector
+    ax.scatter(ef1, ef2, c=map(weightcolor, Dn), s=200)
+    # plot scaled truth
+    truth_ = truth * (1.2 * rmax(ef1, ef2))
+    truthx, truthy = zip(*truth_)
+    ax.scatter(truthx, truthy, c=map(weightcolor, Dn), s=200)
+    # plot edges for (ef1,ef2) vertices
     for i in range(N):
         for j in range(i+1, N):
             if W[i,j] > 0.0:
-                c = W[i,j]
                 xs = [ ef1[i], ef1[j] ]
                 ys = [ ef2[i], ef2[j] ]
-                ax.plot(xs, ys, lw=1.4, c=Wcolor(c))
+                ax.plot(xs, ys, lw=1.4, c=weightcolor(W[i,j]))
 
 
-def show_on_truth(ax, ef1, truth, W, rn=None):
+def show_on_truth(ax, f1, efuns, truth, W, rn=None):
     N, M = W.shape
-    assert N == M and truth.shape == (N, 2)
-    assert len(ef1) == N
+    assert N == M and truth.shape == (N, 2) and efuns.shape == (N, N)
     if rn:
         truth_ = [ (x*r, y*r) for (x,y), r in zip(truth, rnoise(rn, N)) ]
     else:
         truth_ = truth
     truthx, truthy = zip(*truth_)
-    ##ef1 = truthx  #=truthy - a hack!
-    transf = (lambda v: v)  #(lambda v: v**3)
-    ef1cran = max(abs(min(ef1)), abs(max(ef1)))
-    ef1cran = transf(ef1cran)
+    ef1 = efuns[:,f1]
+    ef1cran = max(abs(efuns.min()), abs(efuns.max()))
     ef1norm = colors.Normalize(vmin=-ef1cran, vmax=ef1cran)
-    ef1color = (lambda v: cm.ScalarMappable(norm=ef1norm, cmap=seismic).to_rgba(transf(v)))
-    ef1c = map(ef1color, ef1)
-    ax.scatter(truthx, truthy, c=ef1c, s=200)
-    Wnorm = colors.Normalize(vmin=0.0, vmax=W.max())
-    Wcolor = cm.ScalarMappable(norm=Wnorm, cmap=gray_r).to_rgba
+    ax.scatter(truthx, truthy, c=map(ef1color(ef1norm), ef1), s=200)
     for i in range(N):
         for j in range(i+1, N):
             if W[i,j] > 0.0:
-                c = W[i,j]
                 xs, ys = zip(truth_[i], truth_[j])
-                ax.plot(xs, ys, lw=1.4, c=Wcolor(c))
+                ax.plot(xs, ys, lw=1.4, c=weightcolor(W[i,j]))
 
 
 if __name__ == "__main__":
@@ -91,14 +102,12 @@ if __name__ == "__main__":
     evals = np.loadtxt(path.join(exp_dir, "evals"))
     W = np.loadtxt(path.join(exp_dir, "W"))
     D = np.loadtxt(path.join(exp_dir, "D"))
-
-    
     
     if not args.truth:
-        show_efuns(plt, efuns[args.f1], efuns[args.f2], W, D)
-        plt.gray()
+        fig, ax = plt.subplots(1, 1)
+        show_efuns(ax, args.f1, args.f2, efuns, truth, W, D)
+        ax.set_axis_bgcolor("lightgreen")
     else:
         fig, ax = plt.subplots(1, 1)
-        show_on_truth(ax, efuns[args.f1], truth, W, rn=args.rnoise)
-        ax.set_axis_bgcolor("lightgreen")
+        show_on_truth(ax, args.f1, efuns, truth, W, rn=args.rnoise)
     plt.show()
